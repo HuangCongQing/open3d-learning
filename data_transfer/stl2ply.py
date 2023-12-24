013e8ffa758a3a089e294343ecf19cf498e3180a
hcq <1756260160@qq.com>
2023-12-25 01:08:24 +0800

    feat: stl_to_ply

--- data_transfer/stl2ply.py
+++ data_transfer/stl2ply.py
@@ -4,7 +4,7 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2023-12-24 23:42:35
-LastEditTime: 2023-12-24 23:51:17
+LastEditTime: 2023-12-25 01:07:08
 FilePath: /open3d-learning/data_transfer/stl2ply.py
 '''
 import open3d as o3d
@@ -13,18 +13,11 @@ def stl_to_ply(stl_file, ply_file):
     # 读取STL点云文件
     mesh = o3d.io.read_triangle_mesh(stl_file)
 
-    # 提取点云数据
-    pcd = mesh.sample_points_poisson_disk(10000)  # 使用Poisson-Disk采样提取点云数据
-
-    # 添加注释
-    colors = [[1, 0, 0] for _ in range(pcd.points.shape[0])]  # 创建红色注释
-    pcd.colors = o3d.utility.Vector3dVector(colors)  # 设置点云注释
-
-    # 保存为ply文件
-    o3d.io.write_point_cloud(ply_file, pcd)
+     # 将网格保存为 PLY 文件  
+    o3d.io.write_triangle_mesh(ply_file, mesh) 
 
 # 使用示例
-stl_file = '/home/hcq/pointcloud/open3d-learning/data_transfer/output.stl'   # 输入的STL点云文件路径
+stl_file = '/home/hcq/pointcloud/open3d-learning/test_data/stl/Dragon/Dragon 2.5_stl.stl'   # 输入的STL点云文件路径
 ply_file = 'output.ply'  # 输出的PLY点云文件路径
 
 stl_to_ply(stl_file, ply_file)
