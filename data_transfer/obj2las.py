d07001e18ddd6d35018f0343cc45b0205f4ce20f
hcq <1756260160@qq.com>
2023-12-24 19:42:50 +0800

    TODO: error

--- data_transfer/obj2las.py
+++ data_transfer/obj2las.py
@@ -4,7 +4,7 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2023-12-24 16:48:39
-LastEditTime: 2023-12-24 17:33:15
+LastEditTime: 2023-12-24 19:42:43
 FilePath: /open3d-learning/data_transfer/obj2las.py
 '''
 import open3d as o3d
@@ -30,6 +30,7 @@ las_file = "../test_data/obj/FinalBaseMesh_output.las"
 convert_obj_to_las(obj_file, las_file)
 
 '''  
+issue: https://github.com/isl-org/Open3D/issues/3166
 [Open3D WARNING] Unable to load file ../test_data/obj/FinalBaseMesh.obj with ASSIMP
 [Open3D WARNING] Write geometry::PointCloud failed: unknown file extension las for file ../test_data/obj/FinalBaseMesh_output.las.
 '''
