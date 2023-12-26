e5ab45b2b160e3ed03b8babad7f62323427781e5
hcq <1756260160@qq.com>
2023-12-26 19:53:44 +0800

    feat: 不同格式文件读取函数合集

---
+++ data_transfer/utils.py
@@ -0,0 +1,28 @@
+'''
+Description: 不同格式文件读取函数合集(可根据需要选取部分函数自取)
+Author: HCQ
+Company(School): UCAS
+Email: 1756260160@qq.com
+Date: 2023-12-26 19:45:21
+LastEditTime: 2023-12-26 19:52:24
+FilePath: /open3d-learning/data_transfer/utils.py
+'''
+import open3d as o3d  
+import numpy as np
+
+# txt
+def read_txt(file_path):
+    pass
+def save_txt(points, file_path):
+    pass
+# txt
+def read_pcd(file_path):
+    pass
+def save_pcd(points, file_path):
+    pass
+# txt
+def read_ply(file_path):
+    pass
+def save_ply(points, file_path):
+    pass
+
